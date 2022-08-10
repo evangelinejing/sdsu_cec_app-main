8551699af9d65874b934c9197130e906b89085b7
evangelinejing <jmzevangeline@yahoo.com>
2022-08-10 14:42:43 -0700

    User Control

---
+++ homehome/UserControl/UserControl.swift
@@ -0,0 +1,20 @@
+//
+//  UserControl.swift
+//  homehome
+//
+//  Created by Evangeline Jing on 8/10/22.
+//
+
+import SwiftUI
+
+struct UserControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserControl_Previews: PreviewProvider {
+    static var previews: some View {
+        UserControl()
+    }
+}
