bd3968d1231cd0332f27854b73970ac007c292be
evangelinejing <jmzevangeline@yahoo.com>
2022-08-09 22:01:21 -0700

    HOMEPAGE UPDATED

--- homehome/MAIN/HPListView.swift
+++ homehome/MAIN/HPListView.swift
@@ -38,9 +38,9 @@ struct HPListView: View {
             List(datas, id: \.id){ data in
                 NavigationLink(destination: HPDetail(data:data),label:{
                     HStack{
-                        Text(data.title).font(.title).foregroundColor(Color(hue: 0.9, saturation: 0.078, brightness: 0.959)).dynamicTypeSize(.xLarge).menuStyle(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=Menu Style@*/DefaultMenuStyle()/*@END_MENU_TOKEN@*/)
+                        Text(data.title).font(.title).foregroundColor(Color(.black).opacity(0.7)).dynamicTypeSize(.xLarge).menuStyle(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=Menu Style@*/DefaultMenuStyle()/*@END_MENU_TOKEN@*/)
                         Image(systemName: data.imageName)
-                            .font(.title).foregroundColor(Color(hue: 0.9, saturation: 0.078, brightness: 0.959)).dynamicTypeSize(.xLarge).menuStyle(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=Menu Style@*/DefaultMenuStyle()/*@END_MENU_TOKEN@*/).padding(.vertical)
+                            .font(.title).foregroundColor(Color(.black).opacity(0.7)).dynamicTypeSize(.xLarge).menuStyle(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=Menu Style@*/DefaultMenuStyle()/*@END_MENU_TOKEN@*/).padding(.vertical)
                     }.background(
                         RoundedRectangle (cornerRadius:10, style:.continuous).fill (.linearGradient (colors:[.black.opacity(0.15)], startPoint: .top, endPoint: .bottomTrailing)))
                 })
