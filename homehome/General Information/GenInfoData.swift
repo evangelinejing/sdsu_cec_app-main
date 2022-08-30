7d697ce5e605bd1519f4c8cfa41f068570a7df54
evangelinejing <jmzevangeline@yahoo.com>
2022-08-29 22:30:47 -0700

    add general info page

--- homehome/General Information/GenInfoData.swift
+++ homehome/General Information/GenInfoData.swift
@@ -8,23 +8,21 @@
 import SwiftUI
 import SwiftUICharts
 
+//
+
 struct GenInfoData: View {
     var body: some View {
         VStack{
             BarChartView(
                 data: ChartData (values: [
                     ("PV", 12),
-                    ("Load", 22),
-                    ("Grid", 25),
-                    ("BATT", 23),
-                    
+                    ("Load", -12),
+                    ("Grid", 16),
+                    ("BATT", -16),
                 ]),
                 title: "Site Name: Eton Bio"
             )
-            
-
             Text("DC Bus:")
-
             Text("Demand Power: 5.6kW (the last 15min)")
         }
     }
